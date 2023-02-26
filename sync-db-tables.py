f02fd78eab238b62632f2133a3d164d39258fb45
Daniel Iser <daniel@code-atlantic.com>
2023-02-26 04:32:00 -0500

    Simplify logging, separate methods

--- sync-db-tables.py
+++ sync-db-tables.py
@@ -16,6 +16,18 @@ parser.add_argument('-o', '--output-path', type=str, default=os.path.join(script
 parser.add_argument('-v', '--verbose', action='store_true', help='should the script print verbose output')
 args = parser.parse_args()
 
+# Log messages if verbose is True
+def log(message: str):
+    """Logs a message if verbose is True.
+
+    Args:
+        message (str): The message to log.
+        verbose (bool): Whether or not to log the message.
+    """
+
+    if args.verbose:
+        print(message)
+
 # Open the configuration file and load the database and table information
 with open(args.config, 'r') as config_file:
     config = json.load(config_file)
@@ -27,9 +39,16 @@ def save_config():
     with open(args.config, 'w') as config_file:
         json.dump(config, config_file, indent=4)
 
-
-# Given table info, determine output filename.
 def get_output_filename(table_info):
+    """Returns the output filename for the table.
+
+    Args:
+        table_info (dict): The table information from the config file.
+
+    Returns:
+        str: The output filename for the table.
+    """
+
     if 'output' in table_info:
         return os.path.join(args.output_path, '{}'.format(table_info['output']))
     else:
@@ -37,6 +56,15 @@ def get_output_filename(table_info):
 
 # Check if table file exists, if so get last_id from the file id column, otherwise check if last_id set in config, otherwise return 0
 def get_last_id(table_info):
+    """Returns the last id for the table.
+
+    Args:
+        table_info (dict): The table information from the config file.
+
+    Returns:
+        int: The last id for the table.
+    """
+
     if 'last_id' in table_info:
         return table_info['last_id']
     elif os.path.exists(get_output_filename(table_info)):
@@ -44,110 +72,166 @@ def get_last_id(table_info):
     else:
         return 0
 
+def create_output_file_if_not_exists(table_info):
+    """Creates the output file if it doesn't exist.
+
+    Args:
+        table_info (dict): The table information from the config file.
+    """
+
+    # Check if CSV exists, if not create it with header row by querying table schema
+    if not os.path.exists(get_output_filename(table_info)):
+        # Get table schema
+        table_schema = database.run_query('DESCRIBE {}'.format(table_name))
+
+        # Print the table name to the console with new line.
+        log( "".join(['\r\nTable name: ', format( table_name ) ]))
+
+        # Print the fields in comma list to the console
+        log( "".join(['\r\nTable schema:', ', '.join(table_schema['Field'])]) )
+
+        # Create a CSV file and write the header row, create path if it doesn't exist
+        if not os.path.exists(os.path.dirname(get_output_filename(table_info))):
+            os.makedirs(os.path.dirname(get_output_filename(table_info)))
+
+        with open(get_output_filename(table_info), 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(table_schema['Field'])
+            csv_file.close()
+
+def fetch_rows_from_table(database, db_name:str, table_name: str, table_info, rows_to_extract:int):
+    """Fetches rows from the table and writes them to the CSV file.
+
+    Args:
+        table_info (dict): The table information from the config file.
+    """
+
+    # Name of the CSV file to create for this table
+    csv_filename = get_output_filename(table_info)
+
+    # Get last id from stored table info
+    last_id = get_last_id(table_info)
+
+    # Open CSV file in append mode
+    with open(csv_filename, 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        # Set up the initial offset and limit for fetching rows
+        offset = 0
+        limit = 1000
+        starting_id = last_id
+
+        if args.verbose:
+            pbar = tqdm(total=rows_to_extract)
+        else:
+            pbar = None
+
+        # Loop through the table, fetching rows in chunks and writing them to the CSV file
+        while offset < rows_to_extract:
+            if starting_id:
+                rows = database.run_query('SELECT * FROM {} WHERE id > {} LIMIT {} OFFSET {}'.format(table_name, starting_id, limit, offset))
+            else:
+                rows = database.run_query('SELECT * FROM {} LIMIT {} OFFSET {}'.format(table_name, limit, offset))
+
+            if rows.empty:
+                break
+
+            # Write the rows to the CSV file
+            csv_writer.writerows(rows.values)
+
+            # # Get the last id from the rows
+            last_id = rows['id'].max()
+
+            # # Update the last synced id in the config file
+            config[db_name]['tables'][table_name]['last_id'] = int(last_id)
+
+            # Update the progress bar
+            if pbar is not None:
+                pbar.update(len(rows))
+
+            # Save the config file
+            save_config()
+
+            # Increment the offset
+            offset += limit
+
+        if pbar is not None:
+            pbar.close()
+
+    log( 'Finished fetching rows from table.' )
+
+def verify_csv_file(csv_filename:str, total_rows:int):
+    """Verifies that the CSV file has the correct number of rows.
+
+    Args:
+        csv_file (str): The path to the CSV file.
+        total_count (int): The total number of rows in the table.
+
+    Returns:
+        bool: True if the CSV file has the correct number of rows, False otherwise.
+    """
+
+    # Get the number of rows in the CSV file without header row
+    csv_rows = pd.read_csv(csv_filename).shape[0]
+    if csv_rows == total_rows:
+        log( 'CSV file contains all rows from the table.' )
+
+        return True
+    else:
+        log( 'WARNING: CSV file contains {} rows, but table contains {} rows.'.format(csv_rows, total_rows) )
+        log( 'NOTE: This could be due to data being added to the table while the script is running.' )
+
+        return False
+
 # Loop through each database and table in the configuration
 for db_name, db_info in config.items():
     database = MySQLDB(db_info)
 
     try:
-        print( 'Connecting to database {}...'.format(db_name) )
+        log( 'Connecting to database {}...'.format(db_name) )
+
         # Connect to the database
         database.connect()
 
         # Loop through each table in the database and extract the data
         for table_name, table_info in db_info['tables'].items():
             # Name of the CSV file to create for this table
-            csv_filename = get_output_filename(table_info=table_info)
+            csv_filename = get_output_filename(table_info)
 
             # Get last id from stored table info
-            last_id = get_last_id(table_info=table_info)
+            last_id = get_last_id(table_info)
 
             # Get the total number of rows in the table
             total_rows = database.run_query('SELECT COUNT(*) FROM {}'.format(table_name)).iloc[0,0]
 
-            print( 'Found {} rows in {} table...'.format(total_rows, table_name) )
+            log( 'Found {} rows in {} table...'.format(total_rows, table_name) )
 
             # Print the number of rows that will be extracted
             if last_id:
                 rows_to_extract = database.run_query('SELECT COUNT(*) FROM {} WHERE id > {}'.format(table_name, last_id)).iloc[0,0]
-                print( 'Found {} new rows to extract...'.format(rows_to_extract) )
+                log( 'Found {} new rows to extract...'.format(rows_to_extract) )
             else:
-                print( 'Extracting all rows...' )
                 rows_to_extract = total_rows
+                log( 'Extracting all rows...' )
 
-            print()
+            # Create the output file if it doesn't exist
+            create_output_file_if_not_exists(table_info)
 
-            # Check if CSV exists, if not create it with header row by querying table schema
-            if not os.path.exists(csv_filename):
-                # Get table schema
-                table_schema = database.run_query('DESCRIBE {}'.format(table_name))
+            # Fetch rows from the table and write them to the CSV file
+            fetch_rows_from_table(database, db_name, table_name, table_info, rows_to_extract)
 
-                print()
-                # Print the fields in comma list to the console
-                print( 'Table schema:', ', '.join(table_schema['Field']) )
-                print()
+            # Verify the CSV file has the correct number of rows
+            verify_csv_file(csv_filename, total_rows)
 
-                # Create a CSV file and write the header row, create path if it doesn't exist
-                if not os.path.exists(os.path.dirname(csv_filename)):
-                    os.makedirs(os.path.dirname(csv_filename))
-
-                with open(csv_filename, 'w', newline='') as csv_file:
-                    csv_writer = csv.writer(csv_file)
-                    csv_writer.writerow(table_schema['Field'])
-
-            # Open CSV file in append mode
-            with open(csv_filename, 'a', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-
-                # Set up the initial offset and limit for fetching rows
-                offset = 0
-                limit = 1000
-                starting_id = last_id
-
-                # Loop through the table, fetching rows in chunks and writing them to the CSV file
-                # Wrap the loop with tqdm to add a progress bar
-                with tqdm(total=rows_to_extract) as pbar:
-                    # Loop through the table, fetching rows in chunks and writing them to the CSV file
-                    while offset < rows_to_extract:
-                        if starting_id:
-                            rows = database.run_query('SELECT * FROM {} WHERE id > {} LIMIT {} OFFSET {}'.format(table_name, starting_id, limit, offset))
-                        else:
-                            rows = database.run_query('SELECT * FROM {} LIMIT {} OFFSET {}'.format(table_name, limit, offset))
-
-                        if rows.empty:
-                            break
-
-                        # for each row, write it to the CSV file, headers already exist
-                        for row in rows.itertuples(index=False):
-                            csv_writer.writerow(row)
-                            last_id = row[0]
-
-                            # Update the last synced id in the config file
-                            config[db_name]['tables'][table_name]['last_id'] = int(last_id)
-
-                        pbar.update(len(rows))
-
-                        # Save the config file
-                        save_config()
-
-                        offset += limit
-
-        # Wrote all rows to CSV file, print a message verifying the number of rows written to the file
-        print( 'Wrote {} rows to {}...'.format(rows_to_extract, table_info['output']) )
-
-        # Verify the number of rows in the CSV file matches the number of rows in the table.
-        csv_rows = pd.read_csv(csv_filename, header=None).shape[0]
-        if csv_rows == total_rows:
-            print( 'CSV file contains all rows from the table.' )
-        else:
-            print( 'WARNING: CSV file contains {} rows, but table contains {} rows.'.format(csv_rows, total_rows) )
-            print( 'NOTE: This could be due to data being added to the table while the script is running.' )
+        log( 'Wrote {} rows to {}...'.format(rows_to_extract, table_info['output']) )
 
         # Disconnect from the database
         database.disconnect()
 
     except Exception as e:
-        print(f"Error connecting to database: {e}")
         database.disconnect()
+
+        log(f"{e}")
 
         # Save the config file
         save_config()
