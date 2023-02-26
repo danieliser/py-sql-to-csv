0e955118e0c8fe1e72c20659b60723436d324177
Daniel Iser <daniel@code-atlantic.com>
2023-02-25 23:20:37 -0500

    Improvements to config & output paths

--- sync-db-tables.py
+++ sync-db-tables.py
@@ -7,6 +7,7 @@ import os
 import argparse
 import csv
 from sshtunnel import SSHTunnelForwarder
+from tqdm import tqdm
 
 # Get the directory where the script is located
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -14,6 +15,7 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 # Define the command-line arguments
 parser = argparse.ArgumentParser(description='Extract data from MySQL tables to CSV files.')
 parser.add_argument('--config', type=str, default=os.path.join(script_dir, 'config.json'), help='the configuration file to use')
+parser.add_argument('--output-path', type=str, default=os.path.join(script_dir, 'output'), help='the folder to output to')
 args = parser.parse_args()
 
 # Open the configuration file and load the database and table information
@@ -87,6 +89,23 @@ def save_config():
     with open(args.config, 'w') as config_file:
         json.dump(config, config_file, indent=4)
 
+
+# Given table info, determine output filename.
+def get_output_filename(table_info):
+    if 'output' in table_info:
+        return os.path.join(args.output_path, '{}'.format(table_info['output']))
+    else:
+        return os.path.join(args.output_path, '{}_{}.csv'.format(db_name, table_name))
+
+# Check if table file exists, if so get last_id from the file id column, otherwise check if last_id set in config, otherwise return 0
+def get_last_id(table_info):
+    if os.path.exists(get_output_filename(table_info)):
+        return pd.read_csv(get_output_filename(table_info))['id'].max()
+    elif 'last_id' in table_info:
+        return table_info['last_id']
+    else:
+        return 0
+
 # Loop through each database and table in the configuration
 for db_name, db_info in config.items():
 
@@ -100,13 +119,10 @@ for db_name, db_info in config.items():
     # Loop through each table in the database and extract the data
     for table_name, table_info in db_info['tables'].items():
         # Name of the CSV file to create for this table
-        if 'output' in table_info:
-            csv_filename = table_info['output']
-        else:
-            csv_filename = '{}_{}.csv'.format(db_name, table_name)
+        csv_filename = get_output_filename(table_info=table_info)
 
         # Get last id from stored table info
-        last_id = table_info.get('last_id', 0)
+        last_id = get_last_id(table_info=table_info)
 
         # Get the total number of rows in the table
         total_rows = run_query('SELECT COUNT(*) FROM {}'.format(table_name)).iloc[0,0]
@@ -115,11 +131,11 @@ for db_name, db_info in config.items():
 
         # Print the number of rows that will be extracted
         if last_id:
-            print( 'Extracting rows with id > {}...'.format(last_id) )
             rows_to_extract = run_query('SELECT COUNT(*) FROM {} WHERE id > {}'.format(table_name, last_id)).iloc[0,0]
-            print( 'Found {} rows to extract...'.format(rows_to_extract) )
+            print( 'Found {} new rows to extract...'.format(rows_to_extract) )
         else:
             print( 'Extracting all rows...' )
+            rows_to_extract = total_rows
 
         print()
 
@@ -128,10 +144,11 @@ for db_name, db_info in config.items():
             # Get table schema
             table_schema = run_query('DESCRIBE {}'.format(table_name))
 
+            print()
+
             # Print the fields in comma list to the console
             print( 'Table schema:', ', '.join(table_schema['Field']) )
             print()
-
 
             # Create a CSV file and write the header row, create path if it doesn't exist
             if not os.path.exists(os.path.dirname(csv_filename)):
@@ -148,34 +165,44 @@ for db_name, db_info in config.items():
             # Set up the initial offset and limit for fetching rows
             offset = 0
             limit = 1000
+            starting_id = last_id
 
             # Loop through the table, fetching rows in chunks and writing them to the CSV file
-            while offset < total_rows:
-                if last_id:
-                    rows = run_query('SELECT * FROM {} WHERE id > {} LIMIT {} OFFSET {}'.format(table_name, last_id, limit, offset))
-                else:
-                    rows = run_query('SELECT * FROM {} LIMIT {} OFFSET {}'.format(table_name, limit, offset))
+            # Wrap the loop with tqdm to add a progress bar
+            with tqdm(total=rows_to_extract) as pbar:
+                # Loop through the table, fetching rows in chunks and writing them to the CSV file
+                while offset < rows_to_extract:
+                    if starting_id:
+                        print('SELECT * FROM {} WHERE id > {} LIMIT {} OFFSET {}'.format(table_name, starting_id, limit, offset))
+                        rows = run_query('SELECT * FROM {} WHERE id > {} LIMIT {} OFFSET {}'.format(table_name, starting_id, limit, offset))
+                    else:
+                        print( 'SELECT * FROM {} LIMIT {} OFFSET {}'.format(table_name, limit, offset))
+                        rows = run_query('SELECT * FROM {} LIMIT {} OFFSET {}'.format(table_name, limit, offset))
 
-                if rows.empty:
-                    break
+                    if rows.empty:
+                        break
 
-                # print the first 5 rows nicely to the console for review
-                print( rows.head() )
+                    # for each row, write it to the CSV file, headers already exist
+                    for row in rows.itertuples(index=False):
+                        csv_writer.writerow(row)
 
-                # for each row, write it to the CSV file, headers already exist
-                for row in rows.itertuples(index=False):
-                    csv_writer.writerow(row)
+                    # Update the last synced id file with the last `id` value in the table
+                    last_id = rows['id'].max()
 
-                # Update the last synced id file with the last `id` value in the table
-                last_id = rows.iloc[-1]['id']
+                    print()
+                    # print Last synced id: 12345, transfered 1000 rows out of 10000
+                    print( 'Last synced id: {}, transfered {} rows out of {}'.format(last_id, len(rows), rows_to_extract) )
+                    print()
 
-                # Update the last synced id in the config file
-                config[db_name]['tables'][table_name]['last_id'] = int(last_id)
+                    # Update the last synced id in the config file
+                    config[db_name]['tables'][table_name]['last_id'] = int(last_id)
 
-                # Save the config file
-                save_config()
+                    pbar.update(len(rows))
 
-                offset += limit
+                    # Save the config file
+                    save_config()
+
+                    offset += limit
 
     # Close the database connection
     mysql_disconnect()
